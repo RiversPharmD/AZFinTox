3e56d989b3449af867be1487d43ab98e9f6bb5a8
Zach Rivers <zrivers@fredhutch.org>
2022-01-25 09:59:14 -0500

    add align function

--- 00Functions.r
+++ 00Functions.r
@@ -15,10 +15,10 @@
 ## dat = data frame
 ## num = numeric variable
 ## x = any variable
+## vec = vector
 ################################################################################
 
-## Shift Left
-## a number of surveys are scored on a different scale, this shifts them to the
+## Shift Leftare scored on a different scale, this shifts them to the
 ## original scale
 
 shift_left <- function(x) {
@@ -56,28 +56,105 @@ lengthen <- function(dat, x) {
 ## summary of each observation.
 
 discordance <- function(dat, x) {
+
+## Load Packages
   library(dplyr)
-  library(tidyr)
+
 
 ## Creates small dataset
   dat <- dat %>%
-  dplyr::filter(observation == x)
+    dplyr::filter(observation == x)
 
 ## Builds Discordance measure
   dat <- dat %>%
-  mutate(disc = case_when(
-    value_patient == value_caregiver ~ 0,
-    value_patient < value_caregiver ~ -1,
-    value_patient > value_caregiver ~ 1
-  ))
+    mutate(disc = case_when(
+      value_patient == value_caregiver ~ 0,
+      value_patient < value_caregiver ~ -1,
+      value_patient > value_caregiver ~ 1
+    ))
 ## Summarize Discordance measure
   dat_sum <- dat %>%
-  group_by(disc) %>%
-  count()
+    group_by(disc) %>%
+    count()
 
 ## Prepare return list
-dat_list <- list(dat, dat_sum)
+  dat_list <- list(dat, dat_sum)
 
-return(dat_list)
+## Return list
+  return(dat_list)
 
+}
+
+## Align: This function takes the long dataset and a specific observation as
+## inputs, checks for partner concordance, and creates concordance. It uses the
+## max argument to decide if it should set partner values to the max of both. If
+## max is T, then it will set them both to max. If max is F, then it sets them
+## both to the minimum
+
+align <- function(dat, x, max = TRUE) {
+
+## Dependencies
+  library(dplyr)
+
+ifelse(max == TRUE,
+
+  ## True
+  dat <- dat %>%
+  mutate(value_patient = if_else(observation == x,
+    if_else(is.na(value_patient) == T, value_caregiver,
+      if_else(value_patient >= value_caregiver, value_patient,
+        if_else(value_patient < value_caregiver, value_caregiver, value_patient)
+      )
+    ), value_patient
+  )) %>%
+  mutate(value_caregiver = if_else(observation == x,
+    if_else(is.na(value_caregiver) == T, value_patient,
+      if_else(value_caregiver >= value_patient, value_caregiver,
+        if_else(value_caregiver < value_patient, value_patient, value_caregiver)
+      )
+    ), value_caregiver
+  )),
+## False
+dat <- dat %>%
+ mutate(value_patient = if_else(observation == x,
+   if_else(is.na(value_patient) == T, value_caregiver,
+     if_else(value_patient <= value_caregiver, value_patient,
+       if_else(value_patient > value_caregiver, value_caregiver, value_patient)
+     )
+   ), value_patient
+ )) %>%
+ mutate(value_caregiver = if_else(observation == x,
+   if_else(is.na(value_caregiver) == T, value_patient,
+     if_else(value_caregiver <= value_patient, value_caregiver,
+       if_else(value_caregiver > value_patient, value_patient, value_caregiver)
+     )
+   ), value_caregiver
+ )))
+
+ ## Return
+ return(dat)
+}
+
+## extract_missing: This function pulls the partids the correspond with missing
+## observations in a given column. It takes a long dataframe, scans it for NAs
+## in the column named X, and returns a vector of partids.
+
+extract_missing <- function(dat, x) {
+
+  ## Dependencies
+  library(dplyr)
+
+  ## quosure magic that i don't really understand
+  ## (https://rpubs.com/hadley/dplyr-programming)
+  x <- enquo(x)
+
+  ## filters based on X
+  dat <- dat %>%
+    dplyr::filter(is.na(!!x))
+
+  ## Selects vector of partids
+  vec <- dat$partid
+
+  ## returns vector
+  return(vec)
 }
