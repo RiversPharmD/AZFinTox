ab3e41fcf03d65bafb7331a7474012c817934439
Zach Rivers <zrivers@fredhutch.org>
2022-02-18 11:14:07 -0500

    add output and footnote

--- 09PredictorTableOneandCorrelationMatrix.r
+++ 09PredictorTableOneandCorrelationMatrix.r
@@ -8,6 +8,7 @@
 
 ## Depends on:
 ## 08PredictorMissingness.r
+## flextable
 ## gtsummary
 ## tidyverse
 
@@ -21,6 +22,7 @@
 ################################################################################
 
 # Packages______________________________________________________________________
+library(flextable)
 library(gtsummary)
 library(tidyverse)
 source("00Functions.r")
@@ -83,19 +85,48 @@ dyad_vars <- c("income", "children", "marital", "location")
 
 care_table_one <- care_wide %>%
     select(care_vars) %>%
-    tbl_summary(by = location) %>%
+    tbl_summary(
+        by = location,
+        label = list(
+            age ~ "Age",
+            gender ~ "Gender",
+            ethnicity ~ "Ethnicity",
+            race ~ "Race",
+            education ~ "Education",
+            comorbid_sum ~ "Number of Comorbidities"
+        )
+    ) %>%
     add_p() %>%
     add_overall()
 
 pat_table_one <- pat_wide %>%
     select(pat_vars) %>%
-    tbl_summary(by = location) %>%
+    tbl_summary(
+        by = location,
+        label = list(
+            age ~ "Age",
+            gender ~ "Gender",
+            ethnicity ~ "Ethnicity",
+            race ~ "Race",
+            education ~ "Education",
+            comorbid_sum ~ "Number of Comorbidities",
+            stage ~ "Stage at Diagnosis",
+            dx ~ "Disease Site"
+        )
+    ) %>%
     add_p() %>%
     add_overall()
 
 dyad_table_one <- dyad_wide %>%
     select(dyad_vars) %>%
-    tbl_summary(by = location) %>%
+    tbl_summary(
+        by = location,
+        label = list(
+            income ~ "Household Income",
+            children ~ "Presence of Children Under 18 Years Old",
+            marital ~ "Marital Status"
+        )
+    ) %>%
     add_p() %>%
     add_overall()
 
@@ -107,12 +138,31 @@ table_one <- tbl_stack(
         dyad_table_one
     ),
     group_header = c("Patient", "Caregiver", "Dyad")
-)
+) %>%
+    as_flex_table() %>%
+    flextable::footnote(
+        i = c(22, 61), ## manually added numbers
+        j = 2,
+        value = flextable::as_paragraph("Comorbidities include: Heart Disease,
+    High Blood Pressure, Lung Disease, Diabetes, Ulcer or Stomach Disease,
+    Kidney Disease, Liver Disease, Anemia or Other Blood Disease, Depression,
+    Osteoarthritis/Degenerative Arthritis, Back Pain,
+    and Rheumatoid Arthritis"),
+        ref_symbols = "3",
+        part = "body"
+    )
+
+
 
 table_one
 # Data Out______________________________________________________________________
 
 ## Table One--------------------------------------------------------------------
-path <- "OutData/"
+file_path <- "OutData/"
 
-
+flextable::save_as_docx(table_one,
+    path = paste0(
+        file_path,
+        "table_one.docx"
+    )
+)
