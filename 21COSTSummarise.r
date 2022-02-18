e78bcfaa308ffb41cec0679f2c3d5bfbdda3fc29
Zach Rivers <zrivers@fredhutch.org>
2022-02-18 16:59:18 -0500

    switch to gtsummary and update levels

--- 21COSTSummarise.r
+++ 21COSTSummarise.r
@@ -15,360 +15,359 @@
 
 ################################################################################
 
-# Packages
+# Packages______________________________________________________________________
+library(gtsummary)
+library(flextable)
 library(tidyverse)
 source("00Functions.r")
 
-# Functions
+# Functions_____________________________________________________________________
 
-# Data In
+# Data Prep_____________________________________________________________________
 
-## COST
+## Input -----------------------------------------------------------------------
 
-cost <- list(
-    cost_narrow = read_csv(
-        file = "IntData/cost.csv",
-        col_types = "nfffn"
-        # ),
-        # cost_full = read_csv(
-        #    file = "IntData/cost_full.csv"
-    )
+# cost <- read_csv(
+#     file = "IntData/cost_full.csv",
+#     col_types = c("ffnnnnnnnnnnnnnnnnfff")
+# )
+
+dat <- read_csv(
+    file = "IntData/cost.csv"
 )
 
-# Analysis
+## Split files and select columns-----------------------------------------------
+dat <- dat %>%
+    mutate(
+        src = case_when(
+            str_detect(observation, "_p") == TRUE ~ "patient",
+            str_detect(observation, "_c$") == TRUE ~ "caregiver",
+            str_detect(observation, "con_") == TRUE ~ "dyad",
+            TRUE ~ "ERROR"
+        ),
+        type = case_when(
+            str_detect(observation, "sum") |
+                str_detect(observation, "_raw") == TRUE ~ "continuous",
+            str_detect(observation, "lgl") |
+                str_detect(observation, "_dich") == TRUE ~ "binary",
+            str_detect(observation, "cat") == TRUE ~ "categorical",
+            TRUE ~ "ERROR"
+        )
+    ) %>%
+    select(-c(observation, survey)) %>%
+    pivot_wider(
+        names_from = type,
+        values_from = score
+    )
 
-## Initiate Lists and Tibbles
 
-cost_dat_avail <- list()
-dat_list <- list()
-
-df_dat <- tibble()
-
-## Loop over available data
-
+### All observations across timepoints
+tp_list <- list(
+    dat_tp_1 = NA,
+    dat_tp_2 = NA,
+    dat_tp_3 = NA,
+    dat_tp_4 = NA
+)
+dat_paired <- tibble()
 for (i in 1:4) {
-
-    # Read in data to include only those where both dyad members are available
-    dat <- import_available_survey_data(
-        data = cost$cost_narrow,
-        timepoint = i,
-        survey = "COST",
-        obs = "dyad"
-    )
-
-    # Put in list to reuse later
-    cost_dat_avail[[i]] <- dat
-
-
-    # split into numeric, logical, categorical, and factor_concordance
-
-    dat_num <- dat %>%
-        filter(observation %in% c("sum_p", "sum_c", "con_raw"))
-
-    dat_lgl <- dat %>%
-        filter(observation %in% c(
-            "lgl_p", "lgl_c"
-        )) %>%
-        mutate(score = as_factor(score))
-    dat_cat <- dat %>%
-        filter(observation %in% c("cat_p", "cat_c")) %>%
-        mutate(score = as_factor(score))
-
-    dat_con <- dat %>%
-        filter(observation %in% c("con_dich", "con_cat")) %>%
-        mutate(score = as_factor(score))
-    # Analyze numeric
-
-    ## Create con_abs
-
-    con_abs <- dat_num %>%
-        filter(observation == "con_raw") %>%
+    tp_list[[i]] <- dat %>%
+        import_available_survey_data(
+            timepoint = i,
+            survey = "COST",
+            obs = "dyad"
+        ) %>%
         mutate(
-            observation = "con_abs",
-            score = abs(score)
-        )
-
-    ## rejoin con_abs
-    dat_num <- dat_num %>%
-        rbind(con_abs)
-
-    ## Summarise
-
-    dat_num_sum <- dat_num %>%
-        group_by(observation) %>%
-        summarise(
-            mean = mean(score),
-            sd = sd(score)
-        )
-
-    # Analyze factor
-
-    ## Find number of obs
-    num_obs <- length(unique(dat$partid))
-
-    ## Summarise logical
-    dat_lgl_sum <- dat_lgl %>%
-        group_by(observation) %>%
-        count(score) %>%
-        mutate(per = round((n * 100) / num_obs, 2))
-
-    ## Summarise categorical
-
-    dat_cat_sum <- dat_cat %>%
-        group_by(observation) %>%
-        count(score) %>%
-        mutate(per = round((n * 100) / num_obs, 2))
-
-    ## Summarise concordance
-
-    dat_con_sum <- dat_con %>%
-        group_by(observation) %>%
-        count(score) %>%
-        mutate(per = round((n * 100) / num_obs, 2))
-
-    ## Bind back together
-    dat_fact_sum <- rbind(
-        dat_lgl_sum,
-        dat_cat_sum,
-        dat_con_sum
-    )
-    # Put into dataframe
-
-    ## numeric
-
-    ### Lengthen
-
-    dat_num_sum_long <- dat_num_sum %>%
-        pivot_longer(
-            cols = c(mean, sd),
-            names_to = "stat",
-            values_to = "value"
-        )
-
-    ### Widen
-
-    dat_num_sum_wide <- dat_num_sum_long %>%
-        pivot_wider(
-            names_from = c(observation, stat),
-            names_sep = "_",
-            values_from = value
+            redcap_event_name = i,
+            n = 1
         ) %>%
-        mutate(tp = i)
-
-    ## Factor
-
-    ### Lengthen
-
-    dat_fact_sum_long <- dat_fact_sum %>%
-        pivot_longer(
-            cols = c(n, per),
-            names_to = "stat",
-            values_to = "value"
-        )
-
-    ### Widen
-    dat_fact_sum_wide <- dat_fact_sum_long %>%
-        pivot_wider(
-            names_from = c(observation, score, stat),
-            names_sep = "_",
-            values_from = value
-        ) %>%
-        mutate(tp = i)
-    # Join back together
-
-    dat_wide <- dat_fact_sum_wide %>%
-        left_join(dat_num_sum_wide) %>%
-        mutate(n_dyads = num_obs)
-
-    # Pivot again!
-
-    dat_long <- dat_wide %>%
-        pivot_longer(
-            cols = -tp,
-            names_to = "observation",
-            values_to = paste0("value_", i)
-        ) %>%
-        select(-tp)
-
-    # attach to external list
-    dat_list[[i]] <- dat_long
+        select(n, everything())
+    dat_paired <- rbind(dat_paired, tp_list[[i]])
 }
 
 
-dat <- dat_list[[1]] %>%
-    full_join(dat_list[[2]]) %>%
-    full_join(dat_list[[3]]) %>%
-    full_join(dat_list[[4]]) %>%
-    mutate(across(where(is.numeric), ~ round(., digits = 2))) %>%
-    mutate(across(where(is.numeric), ~ case_when(
-        is.na(.) == TRUE ~ 0,
-        is.na(.) == FALSE ~ .
-    ))) %>%
-    arrange(observation)
-# Tidy
 
-## set up data to split it
+dat_pat <- dat_paired %>%
+    filter(src == "patient")
 
-dat <- dat %>%
-    mutate(type = case_when(
-        str_detect(
-            string = observation,
-            pattern = "mean"
-        ) == T ~ "cont",
-        str_detect(
-            string = observation,
-            pattern = "sd"
-        ) == T ~ "cont",
-        str_detect(
-            string = observation,
-            pattern = "dyads"
-        ) ~ "other",
-        TRUE ~ "cat"
-    ))
+dat_care <- dat_paired %>%
+    filter(src == "caregiver")
 
-## Split data
-dat_other <- dat %>%
-    filter(type == "other")
+dat_dyad <- dat_paired %>%
+    filter(src == "dyad")
 
-dat_cont <- dat %>%
-    filter(type == "cont")
+### All dyads with observations across all timepoints
+dat_all_time_points <- dat_paired %>%
+    filter(partid %in% tp_list[[4]]$partid) %>%
+    filter(partid %in% tp_list[[3]]$partid) %>%
+    filter(partid %in% tp_list[[2]]$partid) %>%
+    filter(partid %in% tp_list[[1]]$partid)
 
-dat_cat <- dat %>%
-    filter(type == "cat")
+all_tp_list <- list(
+    dat_all_tp_1 = NA,
+    dat_all_tp_2 = NA,
+    dat_all_tp_3 = NA,
+    dat_all_tp_4 = NA
+)
 
-## Tidy
+dat_all_paired <- tibble()
+for (i in 1:4) {
+    all_tp_list[[i]] <- dat_all_time_points %>%
+        filter(redcap_event_name == i) %>%
+        select(n, everything())
+    dat_all_paired <- rbind(dat_all_paired, all_tp_list[[i]])
+}
 
-## Categorical
+dat_all_pat <- dat_all_paired %>%
+    filter(src == "patient")
 
-#### Pivot longer
+dat_all_care <- dat_all_paired %>%
+    filter(src == "caregiver")
 
-dat_cat_long <- dat_cat %>%
-    select(-type) %>%
-    pivot_longer(
-        cols = -observation,
-        names_to = "timepoint",
-        names_prefix = "value_",
-        values_to = "value"
-    )
+dat_all_dyad <- dat_all_paired %>%
+    filter(src == "dyad")
 
-#### Separate Observation
+### Table Labels
+pc_labels <- list(
+    n ~ "Number of Participants",
+    continuous ~ "Raw COST Score",
+    binary ~ "Financial Toxicity",
+    categorical ~ "Severity of Financial Toxicity"
+)
 
-dat_cat_long <- dat_cat_long %>%
-    separate(
-        col = observation,
-        into = c("type", "source", "score", "stat"),
-        sep = "\\_"
-    )
+dyad_labels <- list(
+    n ~ "Number of Participants",
+    continuous ~ "Difference in Cost Score",
+    binary ~ "Difference in Financial Toxicity",
+    categorical ~ "Dfference in Severity of Financial Toxicity"
+)
 
-#### Pivot wider
+# Analysis______________________________________________________________________
 
-dat_cat_wide <- dat_cat_long %>%
-    pivot_wider(
-        names_from = stat,
-        values_from = value
-    )
+## Summary Tables---------------------------------------------------------------
 
-#### Staple together
+### By Observation
 
-dat_cat_wide <- dat_cat_wide %>%
-    mutate(obs = paste0(n, " (", per, ")")) %>%
-    select(-c(n, per))
-
-#### Widen by timepoint
-
-dat_cat_wide <- dat_cat_wide %>%
-    pivot_wider(
-        names_from = timepoint,
-        names_prefix = "timepoint_",
-        values_from = obs
-    )
-
-#### Mutate variable names, add sort for sorting
-
-dat_cat_wide <- dat_cat_wide %>%
+#### All available observations
+sum_pat <- dat_pat %>%
+    select(-c(partid, src)) %>%
     mutate(
-        type = if_else(type == "con", source, type),
-        type = if_else(type == "dich", "lgl", type),
-        source = if_else(source %in% c("cat", "lgl", "dich"), "con", source),
-        sort = 3
-    )
-
-### Numeric
-
-#### Pivot longer
-dat_cont_long <- dat_cont %>%
-    select(-type) %>%
-    pivot_longer(
-        cols = -observation,
-        names_to = "timepoint",
-        names_prefix = "value_",
-        values_to = "value"
-    )
-
-#### Separate observation
-
-dat_cont_long <- dat_cont_long %>%
-    separate(
-        col = observation,
-        into = c("type", "source", "stat"),
-        sep = "_"
-    )
-
-#### Pivot Wider
-
-dat_cont_wide <- dat_cont_long %>%
-    pivot_wider(
-        names_from = stat,
-        values_from = value
-    )
-
-#### staple together
-
-dat_cont_wide <- dat_cont_wide %>%
-    mutate(obs = paste0(mean, " (", sd, ")")) %>%
-    select(-c(mean, sd))
-
-#### Pivot again
-
-dat_cont_wide <- dat_cont_wide %>%
-    pivot_wider(
-        names_from = timepoint,
-        names_prefix = "timepoint_",
-        values_from = obs
-    )
-
-#### Rename columns, add Score for sorting
-dat_cont_wide <- dat_cont_wide %>%
-    mutate(
-        type = "num",
-        source = if_else(source %in% c("abs", "raw"), "con", source),
-        score = NA_integer_,
-        sort = 2
-    )
-
-### Sample size
-dat_other_wide <- dat_other %>%
-    mutate(
-        source = "dyad",
-        type = "num",
-        score = NA_integer_,
-        sort = 1
+        binary = factor(binary,
+            levels = c(0, 1),
+            labels = c("No", "Yes")
+        ),
+        categorical = factor(categorical,
+            levels = c(0:3),
+            labels = c("None", "Mild", "Moderate", "Severe")
+        )
     ) %>%
-    rename(
-        "timepoint_1" = value_1,
-        "timepoint_2" = value_2,
-        "timepoint_3" = value_3,
-        "timepoint_4" = value_4
+    tbl_summary(
+        by = redcap_event_name,
+        label = pc_labels,
+        statistic = n ~ "{N}"
     ) %>%
-    select(-observation)
-## Staple them all together
-tidy_dat_sum <- dat_other_wide %>%
-    rbind(dat_cont_wide) %>%
-    rbind(dat_cat_wide) %>%
-    select(source, score, everything()) %>%
-    mutate(source = if_else(source == "con", "z_con", source)) %>%
-    arrange(sort, type, source, score)
+    modify_header(all_stat_cols() ~ "**{level}**")
 
-# Data Out
-write_csv(x = tidy_dat_sum,
-file = "OutData/TidyCostSummary.csv")
+sum_care <- dat_care %>%
+    select(-c(partid, src)) %>%
+    mutate(
+        binary = factor(binary,
+            levels = c(0, 1),
+            labels = c("No", "Yes")
+        ),
+        categorical = factor(categorical,
+            levels = c(0:3),
+            labels = c("None", "Mild", "Moderate", "Severe")
+        )
+    ) %>%
+    tbl_summary(
+        by = redcap_event_name,
+        label = pc_labels,
+        statistic = n ~ "{N}"
+    ) %>%
+    modify_header(all_stat_cols() ~ "**{level}**")
+
+sum_dyad <- dat_dyad %>%
+    select(-c(partid, src)) %>%
+    mutate(
+        binary = factor(binary,
+            levels = c(0:3),
+            labels = c(
+                "No Financial Toxicity",
+                "Both Patient and Caregiver",
+                "Caregiver, Not Patient",
+                "Patient, Not Caregiver"
+            )
+        ),
+        categorical = factor(categorical,
+            levels = c(0:3),
+            labels = c(
+                "No Financial Toxicity",
+                "Equivalent Financial Toxicity",
+                "Caregiver Worse Than Patient",
+                "Patient Worse Than Caregiver"
+            )
+        )
+    ) %>%
+    tbl_summary(
+        by = redcap_event_name,
+        label = dyad_labels,
+        statistic = n ~ "{N}"
+    ) %>%
+    modify_header(all_stat_cols() ~ "**{level}**")
+
+#### Only those with data across all 4 timepoints
+sum_all_pat <- dat_all_pat %>%
+    select(-c(partid, src)) %>%
+    mutate(
+        binary = factor(binary,
+            levels = c(0, 1),
+            labels = c("No", "Yes")
+        ),
+        categorical = factor(categorical,
+            levels = c(0:3),
+            labels = c("None", "Mild", "Moderate", "Severe")
+        )
+    ) %>%
+    tbl_summary(
+        by = redcap_event_name,
+        label = pc_labels,
+        statistic = n ~ "{N}"
+    ) %>%
+    add_p() %>%
+    modify_header(all_stat_cols() ~ "**{level}**")
+
+sum_all_care <- dat_all_care %>%
+    select(-c(partid, src)) %>%
+    mutate(
+        binary = factor(binary,
+            levels = c(0, 1),
+            labels = c("No", "Yes")
+        ),
+        categorical = factor(categorical,
+            levels = c(0:3),
+            labels = c("None", "Mild", "Moderate", "Severe")
+        )
+    ) %>%
+    tbl_summary(
+        by = redcap_event_name,
+        label = pc_labels,
+        statistic = n ~ "{N}"
+    ) %>%
+    add_p() %>%
+    modify_header(all_stat_cols() ~ "**{level}**")
+
+sum_all_dyad <- dat_all_dyad %>%
+    select(-c(partid, src)) %>%
+    mutate(
+        binary = factor(binary,
+            levels = c(0:3),
+            labels = c(
+                "No Financial Toxicity", "Both Patient and Caregiver", "Caregiver, Not Patient",
+                "Patient, Not Caregiver"
+            )
+        ),
+        categorical = factor(categorical,
+            levels = c(0:3),
+            labels = c(
+                "No Financial Toxicity", "Equivalent Financial Toxicity",
+                "Caregiver Worse Than Patient", "Patient Worse Than Caregiver"
+            )
+        )
+    ) %>%
+    tbl_summary(
+        by = redcap_event_name,
+        label = dyad_labels,
+        statistic = n ~ "{N}"
+    ) %>%
+    add_p() %>%
+    modify_header(all_stat_cols() ~ "**{level}**")
+
+### By Timepoint
+
+#### All pairs at each timepoint
+tp_sum_list <- list()
+for (i in 1:4) {
+    dat <- tp_list[[i]]
+    dat_sum <- dat %>%
+        filter(src != "dyad") %>%
+        select(-c(partid, redcap_event_name)) %>%
+        mutate(
+            binary = factor(binary,
+                levels = c(0, 1),
+                labels = c("No", "Yes")
+            ),
+            categorical = factor(categorical,
+                levels = c(0:3),
+                labels = c("None", "Mild", "Moderate", "Severe")
+            )
+        ) %>%
+        tbl_summary(
+            by = src,
+            label = pc_labels,
+            statistic = n ~ "{N}"
+        ) %>%
+        add_p(test = all_continuous() ~ "t.test") %>%
+        modify_header(all_stat_cols() ~ "**{level}**")
+    tp_sum_list[[i]] <- dat_sum
+}
+
+#### Only pairs with data across all timepoints
+all_tp_sum_list <- list()
+for (i in 1:4) {
+    dat <- all_tp_list[[i]]
+    dat_sum <- dat %>%
+        filter(src != "dyad") %>%
+        select(-c(partid, redcap_event_name)) %>%
+        mutate(
+            binary = factor(binary,
+                levels = c(0, 1),
+                labels = c("No", "Yes")
+            ),
+            categorical = factor(categorical,
+                levels = c(0:3),
+                labels = c("None", "Mild", "Moderate", "Severe")
+            )
+        ) %>%
+        tbl_summary(
+            by = src,
+            label = pc_labels,
+            statistic = n ~ "{N}"
+        ) %>%
+        add_p(test = all_continuous() ~ "t.test") %>%
+        modify_header(all_stat_cols() ~ "**{level}**")
+    all_tp_sum_list[[i]] <- dat_sum
+}
+
+## Joined Tables ---------------------------------------------------------------
+sum_src <- tbl_stack(
+    tbls = list(sum_pat, sum_care, sum_dyad),
+    group_header = c("Patient", "Caregiver", "Dyad")
+)
+sum_all_src <- tbl_stack(
+    tbls = list(sum_all_pat, sum_all_care, sum_all_dyad),
+    group_header = c("Patient", "Caregiver", "Dyad")
+)
+sum_tp <- tbl_merge(
+    tbls = tp_sum_list,
+    tab_spanner = c(
+        "Timepoint 1",
+        "Timepoint 2",
+        "Timepoint 3",
+        "Timepoint 4"
+    )
+)
+
+sum_all_tp <- tbl_merge(
+    tbls = all_tp_sum_list,
+    tab_spanner = c(
+        "Timepoint 1",
+        "Timepoint 2",
+        "Timepoint 3",
+        "Timepoint 4"
+    )
+)
+
+sum_src
+sum_all_src
+sum_tp
+sum_all_tp
