b71ef95a159109c8129f591d2974a9cb49626670
Zach Rivers <zrivers@fredhutch.org>
2022-02-23 18:01:36 -0500

    add documentation

---
+++ Functions/sum_survey_by_tp.r
@@ -0,0 +1,16 @@
+#' Summarise Survey at Timepoint
+#'
+#' Count the number of observations available for a survey at a given timepoint
+
+#' @param dat a dataframe with survey data
+#' @param obs a character descriping the observation we want to summarise by
+#' @param tp a numeric value representing the timepoint of interest
+
+
+tp_sum <- function(dat, obs, tp) {
+    dat_out <- dat %>%
+        dplyr::filter(observation == obs) %>%
+        dplyr::filter(redcap_event_name == tp) %>%
+        dplyr::group_by(survey) %>%
+        dplyr::summarise("tp_{{tp}}" := sum(available))
+}
--- Functions/tp_sum.r
+++
@@ -1,7 +0,0 @@
-tp_sum <- function(dat, obs, tp) {
-    dat_out <- dat %>%
-        filter(observation == obs) %>%
-        filter(redcap_event_name == tp) %>%
-        group_by(survey) %>%
-        summarise("tp_{{tp}}" := sum(available))
-}
