3a80d3bc166704102f863339641c174f3d2ab966
Zach Rivers <zrivers@fredhutch.org>
2022-03-02 16:14:20 -0600

    update functiosn to handle by_cohort analysis

--- Functions/table_one.r
+++ Functions/table_one.r
@@ -1,27 +1,27 @@
 split_by_source <- function(dat) {
     pat_wide <- dat %>%
         filter(source == "Patient") %>%
-        select(
-            partid, location,
-            age, gender, ethnicity, race, education, comorbid_sum,
-            stage, dx,
-            source
-        )
+        select(any_of(c(
+            "partid", "location",
+            "age", "gender", "ethnicity", "race", "education", "comorbid_sum",
+            "stage", "dx",
+            "source", "cohort"
+        )))
 
     care_wide <- dat %>%
         filter(source == "Caregiver") %>%
-        select(
-            partid, location,
-            age, gender, ethnicity, race, education, comorbid_sum,
-            source
-        )
+        select(any_of(c(
+            "partid", "location",
+            "age", "gender", "ethnicity", "race", "education", "comorbid_sum",
+            "source", "cohort"
+        )))
 
     dyad_wide <- dat %>%
         filter(source == "Patient") %>%
-        select(
-            partid, location,
-            income, children, marital
-        )
+        select(any_of(c(
+            "partid", "location",
+            "income", "children", "marital", "cohort"
+        )))
     list_out <- list(
         care_wide,
         pat_wide,
@@ -29,21 +29,38 @@ split_by_source <- function(dat) {
     )
 }
 
-label_vars <- function() {
-    label_vars_out <- list(
-        care_vars <- c(
-            "age", "gender", "ethnicity", "race", "education",
-            "comorbid_sum", "location"
-        ),
-        pat_vars <- c(care_vars, "stage", "dx"),
-        dyad_vars <- c("income", "children", "marital", "location")
-    )
+label_vars <- function(group_var = "location") {
+    if (group_var == "location") {
+        label_vars_out <- list(
+            care_vars <- c(
+                "age", "gender", "ethnicity", "race", "education",
+                "comorbid_sum", "location"
+            ),
+            pat_vars <- c(care_vars, "stage", "dx"),
+            dyad_vars <- c("income", "children", "marital", "location")
+        )
+    } else if (group_var == "cohort") {
+        label_vars_out <- list(
+            care_vars <- c(
+                "age", "gender", "ethnicity", "race", "education",
+                "comorbid_sum", "location", "cohort"
+            ),
+            pat_vars <- c(care_vars, "stage", "dx"),
+            dyad_vars <- c(
+                "income", "children", "marital", "location",
+                "cohort"
+            )
+        )
+    } else {
+        stop("Pick either location or cohort")
+    }
 }
 
 
 label_output <- function() {
     out_list <- list(
         care_labels <- list(
+            location ~ "Study Site",
             age ~ "Age",
             gender ~ "Gender",
             ethnicity ~ "Ethnicity",
@@ -52,6 +69,7 @@ label_output <- function() {
             comorbid_sum ~ "Number of Comorbidities"
         ),
         pat_labels <- list(
+            location ~ "Study Site",
             age ~ "Age",
             gender ~ "Gender",
             ethnicity ~ "Ethnicity",
@@ -62,6 +80,7 @@ label_output <- function() {
             dx ~ "Disease Site"
         ),
         dyad_labels <- list(
+            location ~ "Study Site",
             income ~ "Household Income",
             children ~ "Presence of Children Under 18 Years Old",
             marital ~ "Marital Status"
@@ -70,16 +89,26 @@ label_output <- function() {
 }
 populate_table_one <- function(dat,
                                var,
+                               by = "NONE",
                                label) {
-    dat_out <- dat %>%
-        select(var) %>%
-        tbl_summary(
-            by = location,
-            label = label,
-            missing = "ifany"
-        ) %>%
-        add_p() %>%
-        add_overall()
+    if (by == "NONE") {
+        dat_out <- dat %>%
+            select(var) %>%
+            tbl_summary(
+                label = label,
+                missing = "ifany"
+            )
+    } else {
+        dat_out <- dat %>%
+            select(var) %>%
+            tbl_summary(
+                by = {{ by }},
+                label = label,
+                missing = "ifany"
+            ) %>%
+            add_p() %>%
+            add_overall()
+    }
 }
 
 stack_table_one <- function(list_table_one) {
@@ -95,11 +124,12 @@ footnote_table_one <- function(gt_in, vec_footnote_position) {
         flextable::footnote(
             i = vec_footnote_position, ## manually added numbers
             j = 2,
-            value = flextable::as_paragraph("Comorbidities include: Heart Disease,
-    High Blood Pressure, Lung Disease, Diabetes, Ulcer or Stomach Disease,
-    Kidney Disease, Liver Disease, Anemia or Other Blood Disease, Depression,
-    Osteoarthritis/Degenerative Arthritis, Back Pain,
-    and Rheumatoid Arthritis"),
+            value = flextable::as_paragraph("Comorbidities include:
+            Heart Disease, High Blood Pressure, Lung Disease,
+            Diabetes, Ulcer or Stomach Disease, Kidney Disease,
+            Liver Disease, Anemia or Other Blood Disease, Depression,
+            Osteoarthritis/Degenerative Arthritis, Back Pain,
+            and Rheumatoid Arthritis"),
             ref_symbols = "3",
             part = "body"
         )
